095707378e64d47752c6ef575a6a1df3f13f1be9
cNassim <promail.nassim@gmail.com>
2024-05-27 14:01:55 +0100

    hty

--- django_prject1/django_prject1/settings.py
+++ django_prject1/django_prject1/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = 'django_prject1.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'django_project',
+'USER': 'root',
+'PASSWORD': '',
+'HOST': 'localhost',
+'PORT': '3306',
+}
 }
 
 
