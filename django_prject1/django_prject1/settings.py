0dc26ca30ef380628813012c041afcfa8ad6de42
cNassim <promail.nassim@gmail.com>
2024-05-29 16:09:05 +0100

    lop

--- django_prject1/django_prject1/settings.py
+++ django_prject1/django_prject1/settings.py
@@ -139,3 +139,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
