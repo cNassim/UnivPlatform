12e4766fb00c2bf5ec32991ec59cadd2ecdb16c3
cNassim <promail.nassim@gmail.com>
2024-05-30 16:36:22 +0100

    wa3 teest

--- django_prject1/django_prject1/urls.py
+++ django_prject1/django_prject1/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from etudiant import views
+from etudiant.views import (
+    sing_in, sing_up, log_out,
+    forgot_password, update_password
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,9 +14,13 @@ urlpatterns = [
     path('etudiant/', views.etudiant, name='etudiant'),
     path('candidature/', views.candidature, name='candidature'),
     path('suivis/', views.suivis, name="suivis"),
-    path('', include('account.urls')),  
+    path('', include('account.urls')), 
+    path('login', sing_in, name='sing_in'),
+    path('register', sing_up, name='sing_up'),
+    path('logout', log_out, name='log_out'),
+    path('forgot-password', forgot_password, name='forgot_password'),
+    path('update-password', update_password, name='update_password'),
 
-    
 ]
 
 
--- django_prject1/etudiant/views.py
+++ django_prject1/etudiant/views.py
@@ -1,16 +1,132 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.core.mail import EmailMessage
 
 def home(request):
     return render(request, 'home.html')
-
-def etudiant(request):
-    return render(request, 'etudiant.html')
 
 def candidature(request):
     return render(request, 'candidature.html')
 
 def suivis(request):
     return render(request, 'suivis.html')
+def sing_in(request):
 
+    if request.method == "POST":
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+
+        user = User.objects.filter(email=email).first()
+        if user:
+            auth_user = authenticate(username=user.username, password=password)
+            if auth_user:
+                login(request, auth_user)
+                return redirect('etudiant')
+            else:
+                print("mot de pass incorrecte")
+        else:
+            print("User does not exist")
+
+    return render(request, 'login.html', {})
+
+def sing_up(request):
+    error = False
+    message = ""
+    if request.method == "POST":
+        name = request.POST.get('name', None)
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+        repassword = request.POST.get('repassword', None)
+        # Email
+        try:
+            validate_email(email)
+        except:
+            error = True
+            message = "Enter un email valide svp!"
+        # password
+        if error == False:
+            if password != repassword:
+                error = True
+                message = "Les deux mot de passe ne correspondent pas!"
+        # Exist
+        user = User.objects.filter(Q(email=email) | Q(username=name)).first()
+        if user:
+            error = True
+            message = f"Un utilisateur avec email {email} ou le nom d'utilisateur {name} exist déjà'!"
+        
+        # register
+        if error == False:
+            user = User(
+                username = name,
+                email = email,
+            )
+            user.save()
+
+            user.password = password
+            user.set_password(user.password)
+            user.save()
+
+            return redirect('sing_in')
+
+
+    context = {
+        'error':error,
+        'message':message
+    }
+    return render(request, 'register.html', context)
+
+
+@login_required(login_url='sing_in')
+def etudiant(request):
+    return render(request, 'etudiant.html', {})
+
+def log_out(request):
+    logout(request)
+    return redirect('sing_in')
+
+
+def forgot_password(request):
+    error = False
+    success = False
+    message = ""
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = User.objects.filter(email=email).first()
+        if user:
+            print("processing forgot password")
+            html = """
+                <p> Hello, merci de cliquer pour modifier votre email </p>
+            """
+
+            msg = EmailMessage(
+                "Modification de mot de pass!",
+                html,
+                "soroib0879@gmail.com",
+                ["soro4827@gmail.com"],
+            )
+
+            msg.content_subtype = 'html'
+            msg.send()
+            message = "processing forgot password"
+            success = True
+        else:
+            print("user does not exist")
+            error = True
+            message = "user does not exist"
+    context = {
+        'success': success,
+        'error':error,
+        'message':message
+    }
+    return render(request, "forgot_password.html", context)
+
+
+def update_password(request):
+    return render(request, "update_password.html", {})
