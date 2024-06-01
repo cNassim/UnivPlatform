500934e417013f39b2e46ace767804b06a8b6570
cNassim <promail.nassim@gmail.com>
2024-06-01 03:16:21 +0100

    r

--- django_prject1/superadmin/admin.py
+++ django_prject1/superadmin/admin.py
@@ -8,17 +8,7 @@ class UniversitéAdmin(admin.ModelAdmin):
     list_display = ('id_univ', 'Nom', 'Email', 'Numero_de_telephone', 'adresse', 'pays', 'siteweb')
 
 class FormationAdmin(admin.ModelAdmin):
-    list_display = ('id_formation', 'Nom', 'Description', 'get_university_name')
-
-    def get_university_name(self, obj):
-        return obj.université.Nom
-
-    get_university_name.short_description = 'University'
-
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        queryset = queryset.select_related('université') 
-        return queryset
+    list_display = ('id_formation', 'Nom', 'Description')
 
 class CandidatureAdmin(admin.ModelAdmin):
     list_display = ('id_candidature', 'Nom', 'Prenom', 'Email', 'Date_de_naissance', 'Nationalité', 'Date_de_soumission', 'status1', 'status2', 'status3', 'status4', 'status5', 'status6', 'status7', 'status8')
