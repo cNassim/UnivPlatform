91a8bd320d52c0b83abfa8eff25333c4b3eb0412
cNassim <promail.nassim@gmail.com>
2024-06-03 14:41:45 +0100

    tt

--- django_prject1/agent/models.py
+++ django_prject1/agent/models.py
@@ -1,22 +1,2 @@
 from django.db import models
-
-class Agent(models.Model):
-    nom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    telephone = models.CharField(max_length=15)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.nom} {self.prenom}"
-
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    student_id = models.CharField(max_length=20)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    agent = models.ForeignKey(Agent, on_delete=models.CASCADE, related_name='students')
-
-    def __str__(self):
-        return self.name
+###########################
--- django_prject1/etudiant/models.py
+++ django_prject1/etudiant/models.py
@@ -1,37 +1,5 @@
 from django.db import models
 
-class Agent(models.Model):
-    id_agent = models.AutoField(primary_key=True)
-    Nom = models.CharField(max_length=50)
-    Prenom = models.CharField(max_length=50)
-    Email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    Telephone = models.CharField(max_length=15)
-
-    def _str_(self):
-        return f"{self.Nom} {self.Prenom}"
-
-class Université(models.Model):
-    id_univ = models.AutoField(primary_key=True)
-    Nom = models.CharField(max_length=50)
-    Email = models.EmailField(max_length=50)
-    Numero_de_telephone = models.CharField(max_length=50)
-    adresse = models.CharField(max_length=50)
-    pays = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='univers_photos/', null=True, blank=True)
-    siteweb = models.URLField(max_length=200, null=True, blank=True)
-
-    def _str_(self):
-        return self.Nom
-
-class Formation(models.Model):
-    id_formation = models.AutoField(primary_key=True)
-    Nom = models.CharField(max_length=50)
-    Description = models.CharField(max_length=50)
-
-    def _str_(self):
-        return self.Nom
-
 class Candidature(models.Model):
     id_candidature = models.AutoField(primary_key=True)
     id_etudiant = models.IntegerField()
